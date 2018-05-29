3a4610f094c2ebaed2e918d827871f1ef472b8f0
Will Jordan <will@code.org>
2018-05-29 15:11:41 -0700

    use Sequel::Model for Form class Simplifies model logic in #update_form helper.

--- lib/cdo/db.rb
+++ lib/cdo/db.rb
@@ -46,5 +46,7 @@ end
 
 PEGASUS_DB = sequel_connect CDO.pegasus_db_writer, CDO.pegasus_db_reader
 POSTE_DB = PEGASUS_DB
+Sequel::Model.db = PEGASUS_DB
+
 DASHBOARD_DB = sequel_connect CDO.dashboard_db_writer, CDO.dashboard_db_reader
 DASHBOARD_REPORTING_DB_READER = sequel_connect CDO.dashboard_reporting_db_reader, CDO.dashboard_reporting_db_reader
